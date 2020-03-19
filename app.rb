3fa9e4992d1551e5e05b94e1438470687dd741ef
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-19 04:29:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  
+  get '/newteam' do
+    
+    erb :newteam
+  end 
+  
+  post '/team' do
+   @team = params["team"]
+    @coach = params["coach"]
+    @point_guard = params["point_guard"]
+    @shooting_guard = params["shooting_guard"]
+    @power_forward = params["power_forward"]
+    @small_forward = params["small_forward"]
+    @center = params["center"]
+    
+    erb :team 
+  end
 
 
 end
